86b102af7fd46a67aab2522e7d8c7ea5fe6020b5
Mattie Marano <mwm89@miami.edu>
2019-12-10 14:02:15 -0500

    finishing touches of wanderlust

---
+++ homework/final-project/readme.md
@@ -0,0 +1,12 @@
+# Wanderlust
+
+1. Why are you making this project? What is your motivation?
+  - I chose to create Wanderlust, my creative coding final, with the intent of creating a type of travel diary, allowing me to reminisce on my past travel experiences. I have never been one for journaling or writing in a diary, but I have recently begun to wish I have. There are so many moments and experiences about my past travel experiences that I do not want to forget, and this is my solution to combatting memory loss. 
+2.What are your inputs?
+  - The inputs for this project include the photos of passports, representing my own personal passport. Another main input would be my coded html website "Wanderlust," which displays all the information about each trip, which is shown when each stamp is clicked on.
+3.What are your outputs?
+  - The outputs for Wanderlust is an interactive experience, where users can click through different travel experiences. The information available will be places visited, food eaten, and favorite experiences. These will all be accessible with clickable buttons and hitzones.  
+4.Who is the target audience for your project?
+  - The target audience for Wanderlust is mostly for my own personal use, however it can also be useful for people looking to gain insights into what it is like to travel to Costa Rica, Morocco, Italy and Spain.
+5.Where will your project be displayed?
+  - Wanderlust will be displayed online, specifically on my personal website in the portfolio section. This will allow me to give potential employers a look at my different skills, while also showing more of my personality and interests.
--- homework/final-project/sketch.js
+++ homework/final-project/sketch.js
@@ -1,33 +1,153 @@
+var title;
+var instructions1 = "A Virtual Travel Experience";
+var instructions2 = "Click on one of the stamps to read about that specific trip";
 var frame;
 var passportCover;
 var stampsPage;
 
+
+var changeButton; //button to change to the next screen
+var learnButton; //button to learn about the pproject
+var closeButton; //button to close the passport
+
+var currentImage = 0;
+
+
 function preload() {
    passportCover = loadImage("photos/passport_cover.jpg")
    stampsPage = loadImage("photos/inside_passport.jpg")
+   title = loadImage("photos/wanderlust.png")
 }
 
 function setup() {
    // put setup code here
+   var cnv = createCanvas(800, 800);
+   cnv.id("mycanvas");
+
+   var container0 = createDiv()
+   container0.id("container0");
+   //To search for an ID you use the # token infront of the name
+   //.html() fucntion allows you to allow html (or text) into another html tag
+   select("#container0").style("width", "800px");
+   //0 refers to the top and bottom spacing;the auto refers to the left and right spacing. this will only work if you set a width
+   select("#container0").style("margin", "0 auto");
+   //container 0 is now the parent of the div tag and the h1; the container is the parent of the canvas
+   cnv.parent("#container0");
+   select("body").style("background-color", "white");
+   var container1 = createDiv();
+   //end of centering the project (passportcover)
+
+
+   //creating button to change to the next screen
+   changeButton = new Clickable(500, 500);
+   changeButton.x = 465;
+   changeButton.y = 650;
+   changeButton.width = 150;
+   changeButton.height = 100;
+   changeButton.color = "#71c7ec";
+   changeButton.stroke = "#71c7ec";
+   changeButton.text = "Open Passport";
+   changeButton.textColor = 255;
+   changeButton.textSize = 15;
+
+   //click to learn more about the project
+   learnButton = new Clickable(500, 500);
+   learnButton.x = 230;
+   learnButton.y = 650;
+   learnButton.width = 150;
+   learnButton.height = 100;
+   learnButton.color = "#71c7ec";
+   learnButton.stroke = "#71c7ec";
+   learnButton.text = "What is Wanderlust?";
+   learnButton.textColor = 255;
+   learnButton.textSize = 15;
+
+   //click to close passportCover
+   closeButton = new Clickable(500, 500);
+   closeButton.x = 700;
+   closeButton.y = 750;
+   closeButton.width = 100;
+   closeButton.height = 50;
+   closeButton.color = "#71c7ec";
+   closeButton.stroke = "#71c7ec";
+   closeButton.text = "Close Passport";
+   closeButton.textColor = 255;
+   closeButton.textSize = 15;
+
+
+   //creating canvas for the iframe(html website)
    createCanvas(800, 800);
    frame = createElement("iframe");
    frame.id("frame");
    frame.attribute("src", "https://mattiemarano.github.io/hw/finalProject/index.html")
    frame.attribute("frameborder", "1");
    frame.position(width, 0);
-   frame.size(570, height);
-   frame.show();
+   frame.size(800, 800);
+   frame.hide();
 
 
-}
+   changeButton.onRelease = function() {
+      currentImage = 1;
+   } //changing background image to the stamp
+
+   learnButton.onRelease = function() {
+      alert("Wanderlust is a virtual representation of my travel expereinces over the past four years. This virtual travel diary has allowed me to reminisce on these life changing expereinces, while also sharing them with others. I invite you to click through my passport to read about where i've been, what i've eaten, and my favorite memories abroad over the years. I hope you enjoy the experiences as much as I did, Mattie  Marano");
+   } //end of alert
+
+   closeButton.onRelease = function() {
+      currentImage = 0;
+   } //end of closing passport
+
+} //end of setup
+
 
 function draw() {
    // put drawing code here
-   background(passportCover);
+   if (currentImage == 0) {
+      background(passportCover);
+      frame.hide();
+      changeButton.draw();
+      learnButton.draw();
+      image(title, 175, -50, 450, 200);
+      textSize(30);
+      fill(255);
+      text(instructions1, 410, 130);
 
-}
+
+   } else if (currentImage == 1) {
+      background(stampsPage);
+      closeButton.draw();
+      textSize(30);
+      fill("white");
+      text(instructions2, 400, 50);
+
+      noFill();
+      ellipse(300, 300, 100, 100); //costaRica
+      ellipse(120, 600, 100, 100); //spain
+      ellipse(570, 620, 100, 100); //morocco
+      ellipse(700, 280, 100, 100); //italy
+   }
+
+} //end of draw loop
 
 function mousePressed() {
-   frame.attribute("src", "https://mattiemarano.github.io/hw/responsive/index.html#popular-destinations")
+   if (currentImage == 0) {
 
-}
+   } else if (currentImage == 1) {
+      frame.show();
+
+      if (dist(mouseX, mouseY, 300, 300) < 100) {
+         frame.attribute("src", "https://mattiemarano.github.io/hw/finalProject/costaRica.html");
+      }
+      if (dist(mouseX, mouseY, 120, 600) < 100) {
+         frame.attribute("src", "https://mattiemarano.github.io/hw/finalProject/spain.html");
+      }
+      if (dist(mouseX, mouseY, 570, 620) < 100) {
+         frame.attribute("src", "https://mattiemarano.github.io/hw/finalProject/morocco.html");
+      }
+      if (dist(mouseX, mouseY, 700, 280) < 100) {
+         frame.attribute("src", "https://mattiemarano.github.io/hw/finalProject/italy.html");
+      }
+
+   }
+} //end of mousepressed
